7a734002e07402a90870ef0adb50849edf11915a
louhf <lhfbalance@hotmail.com>
2018-05-27 14:08:02 +0800

    java中的动态语言特性

---
+++ src/main/java/learnMethodHandle/MethodHandleTest.java
@@ -0,0 +1,27 @@
+package learnMethodHandle;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+public class MethodHandleTest {
+
+    static class ClassA {
+        public void println(String a) {
+            System.out.println(a);
+        }
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Object obj = System.currentTimeMillis() % 2 == 0 ? System.out : new ClassA();
+        getPrintlnMH(obj).invokeExact("icyfenix");
+
+    }
+
+    private static MethodHandle getPrintlnMH(Object reveiver) throws Throwable {
+
+        MethodType mt = MethodType.methodType(void.class, String.class);
+        return MethodHandles.lookup().findVirtual(reveiver.getClass(),  "println", mt).bindTo(reveiver);
+    }
+
+}
